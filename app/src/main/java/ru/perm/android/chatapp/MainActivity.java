4c1117f751d3691141ebcc4d8722f90fb831d91c
Николай <niccolum129@gmail.com>
2020-07-22 19:51:58 +0500

    Firebase
    + firebase database

--- app/src/main/java/ru/perm/android/chatapp/MainActivity.java
+++ app/src/main/java/ru/perm/android/chatapp/MainActivity.java
@@ -3,12 +3,45 @@ package ru.perm.android.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference message = database.getReference("messages");
+
+    Button btnSpecial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSpecial = findViewById(R.id.btnSpecial);
+        btnSpecial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                message.push().setValue("Hello, World!");
+            }
+        });
+
+        message.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(getClass().getName(), "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) { }
+        });
     }
 }
